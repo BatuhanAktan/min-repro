a00b9b97edd129e5479098d18e9b4b2883d77574
Batuhan Aktan <batuhanaktan@gmail.com>
2024-07-08 14:58:52 -0400

    Added Dashboard Page Content

---
+++ app/dashboard/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar';
+
+export const Page = () => {
+  return (
+    <>
+    <NavBar />
+    <div>page</div>
+    </>
+  )
+}
+
+export default Page;
